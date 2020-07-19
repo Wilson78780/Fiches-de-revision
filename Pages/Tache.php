e627f6cea8b23d351b5292dc7c80ee3f377d0164
Wilson <huardwilson@icloud.com>
2020-07-19 18:20:13 +0200

    Amélioration

--- Pages/Tache.php
+++ Pages/Tache.php
@@ -89,16 +89,7 @@
             </div>
         </div>
 
-        <footer>
-            <p> <img src="../Photos/CDA_LogoFull.png"> </p>
-            CABINET DE SAINT GERMAIN EN LAYE, QUARTIER BEL-AIR (PROCHE SOUS-PRÉFECTURE) </br>
-            21 avenue Saint Fiacre </br>
-            78100 Saint Germain en Laye </br>
-            <p style="color:#bf6516 ">
-                <a href="mailto:contact@cda-conseils.com" style="text-decoration: none; color:#bf6516;"> contact@cda-conseils.com </a> </br>
-                01 30 61 57 92</p>
-
-        </footer>
+        <?php include "../Style/footer.php" ?>
 
     </body>
 </html>
